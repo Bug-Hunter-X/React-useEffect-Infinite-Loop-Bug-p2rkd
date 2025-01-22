b4f826af39c5f9f096d6b86260b23a13d4958fa6
BugHunter-X <askzenai@gmail.com>
2025-01-22 12:28:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect will run infinitely because it doesn't have a cleanup function
+    console.log('Count changed:', count);
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
